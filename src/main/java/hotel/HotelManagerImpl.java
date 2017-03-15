46d6d30cb199f8bcd574288b12c88163cd3acb9e
František Maša <frantisekmasa1@gmail.com>
2017-03-15 08:16:13 +0100

    Added HotelManagerImpl tests.

---
+++ src/main/java/hotel/HotelManagerImpl.java
@@ -0,0 +1,44 @@
+package hotel;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+public class HotelManagerImpl implements HotelManager
+{
+
+    public Reservation find(long id)
+    {
+        return null;
+    }
+
+    public List<Collection> findCustomerReservations(Customer customer)
+    {
+        return null;
+    }
+
+    public void placeReservation(Reservation reservation)
+    {
+
+    }
+
+    public void delete(Reservation reservation)
+    {
+
+    }
+
+    public List<Reservation> findReservationByRoom(Room room)
+    {
+        return null;
+    }
+
+    public List<Room> findAvailableRooms(Date since, Date until)
+    {
+        return null;
+    }
+
+    public List<Reservation> findAll()
+    {
+        return null;
+    }
+}
