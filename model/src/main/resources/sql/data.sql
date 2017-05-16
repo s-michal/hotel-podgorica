3e29b979c9355a20ba22e72ca0a79eb47ff98958
František Maša <frantisekmasa1@gmail.com>
2017-05-16 11:12:22 +0200

    Create some data on webapp start

---
+++ model/src/main/resources/sql/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO "customer" ("name", "address", "birth_date")
+  VALUES ('František Maša', 'Náměství svobody 666', '1995-10-29');
+
+INSERT INTO "customer" ("name", "address", "birth_date")
+VALUES ('Michal Šustera', 'Cejl 666', '1996-04-11');
+
+INSERT INTO "room" ("number", "floor", "capacity", "price_per_day")
+    VALUES
+      (1, 1, 3, 200),
+      (2, 1, 3, 200),
+      (3, 1, 2, 150),
+      (4, 2, 5, 300),
+      (29, 10, 2, 1500);
